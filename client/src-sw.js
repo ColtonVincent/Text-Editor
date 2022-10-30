5d8b96b82f527aec18c56cdf5bc1b17d7e4884e0
alan <cvinny33@gmail.com>
2022-10-29 18:56:33 -0600

    added asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,14 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({request}) => [].includes(request.destination),
+  new offlineFallback({
+    cacheName: 'page-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+)
